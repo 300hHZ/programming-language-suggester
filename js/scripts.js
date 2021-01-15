6067ee65895755974e096f72bcc3e3bb701ff684
Woo Jin Kim <kimwoojin211@gmail.com>
2021-01-15 11:02:49 -0800

    display different results on form submission. result varies based on answers to first 2 questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+  language = ""; OS = ""; age = ""; fun = "";
 
+  $(questions).submit(function(event){
+    event.preventDefault(); 
+
+    if(language === "")
+    {
+      $("#result").show();
+      $("#description").show();
+    }
+    else
+    {
+      $("."+ language).hide();
+    }
+    $("span#nameOutput").text($("#nameInput").val());
+    language = $("#language").val();
+    console.log(language);
+    $("."+ language).toggle()
+
+  });
 });
