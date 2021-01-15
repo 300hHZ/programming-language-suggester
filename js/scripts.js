489073366fd1e3a2569f4d476455b287608fc114
Woo Jin Kim <kimwoojin211@gmail.com>
2021-01-15 13:52:00 -0800

    make hyperlinks functional, condense css code.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   let language = "";
 
   $(questions).submit(function(event){
-
     event.preventDefault(); 
 
     if(language === "")
@@ -22,6 +21,7 @@ $(document).ready(function(){
     {
       const age = $("input:radio[name=age]:checked").val();
       const os = $("#OS").val();
+
       if ((age === "senior") || (os === "Windows") || (os === "Android"))
       {
         language = "csharp";
@@ -38,7 +38,7 @@ $(document).ready(function(){
       }
     }
     
-
-    $("." + language).show();
-  });
+  $("." + language).show();
+  
+});
 });
